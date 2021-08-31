d649ae04acf649ba16f66c608697ab0a6492db33
יעקב גיגי <yaccovgigi@hotmail.com>
2021-08-31 20:48:33 +0300

    adding setting page with isvisible to UI

--- SmartCounter/SmartCounter/Views/Settings.xaml.cs
+++ SmartCounter/SmartCounter/Views/Settings.xaml.cs
@@ -11,10 +11,29 @@ namespace SmartCounter.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Settings : ContentPage
-    {
+    {     
+        StackLayout stackLayout = new StackLayout();
         public Settings()
         {
             InitializeComponent();
+            stackLayout = (StackLayout)FindByName("SoundCounterIsVisible");
+        }
+
+        private void switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if(e.Value)
+            {
+                stackLayout.IsVisible = true;
+            }
+            else
+            {
+                stackLayout.IsVisible = false;
+            }
+        }
+        
+        private void OnBackFromSettings_Click(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new MainPage());
         }
     }
 }
