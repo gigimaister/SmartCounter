dc83eb8de31a5b18049f2c4b0c716e8304f16e7a
יעקב גיגי <yaccovgigi@hotmail.com>
2021-09-01 16:47:38 +0300

    setting view Is visible on XAML Page

--- SmartCounter/SmartCounter/SplashScreen.cs
+++ SmartCounter/SmartCounter/SplashScreen.cs
@@ -22,28 +22,21 @@ namespace SmartCounter
             var lab = new Label();
             lab.Text = "By Yaccov Gigi";
             lab.FontSize = 35;
-            
-            lab.HorizontalTextAlignment = TextAlignment.Center;
-            lab.VerticalTextAlignment = TextAlignment.Center;
-
+                      
             AbsoluteLayout.SetLayoutFlags(splashImage,
                 AbsoluteLayoutFlags.PositionProportional);           
             AbsoluteLayout.SetLayoutBounds(splashImage,
                 new Rectangle(0.5, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
 
-            sub.Children.Add(splashImage);
             sub.Children.Add(lab);
-            
-
+            sub.Children.Add(splashImage);
+                      
             this.BackgroundColor = Color.FromHex("#E69127");
             this.Content = sub;
-
         }
-
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-
             await splashImage.ScaleTo(1, 2000);
             await splashImage.ScaleTo(0.9, 1500, Easing.Linear);
             await splashImage.ScaleTo(150, 1200, Easing.Linear);
--- SmartCounter/SmartCounter/Views/Settings.xaml.cs
+++ SmartCounter/SmartCounter/Views/Settings.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,13 +10,27 @@ namespace SmartCounter.Views
     {     
         StackLayout stackLayout = new StackLayout();
         Button SettingsButn = new Button();
+        
+
         public Settings()
         {
             InitializeComponent();
+
+            //Init IsSound && Num Of Count Per Sound Settings To Default
+            SwitchSound.IsToggled = Preferences.Get("IsSoundToggle", false);
+            EntryNumOfSoundClick.Text = Preferences.Get("NumOfCountPerSound", "10");
+
+            //Bind To XAML Elements  
             stackLayout = (StackLayout)FindByName("SoundCounterIsVisible");
             SettingsButn = (Button)FindByName("SettingsBtn");
+
+            stackLayout.IsVisible = SettingsButn.IsVisible = SwitchSound.IsToggled;
+            SettingsButn.IsVisible = SwitchSound.IsToggled;
+
         }
 
+        #region Events
+        //Set Sound On
         private void switch_Toggled(object sender, ToggledEventArgs e)
         {
             if(e.Value)
@@ -33,14 +43,22 @@ namespace SmartCounter.Views
                 stackLayout.IsVisible = false;
             }
         }
-        
+        //Back Btn
         private void OnBackFromSettings_Click(object sender, EventArgs e)
         {
             Application.Current.MainPage = new NavigationPage(new MainPage());
         }
+        //SaveSetting Btn
         private void OnBtnSaveSettings_Clicked(object sender, EventArgs e)
         {
+            
+            Preferences.Set("IsSoundToggle", SwitchSound.IsToggled);
+            Preferences.Set("NumOfCountPerSound", EntryNumOfSoundClick.Text);
+
             Application.Current.MainPage = new NavigationPage(new MainPage());
+
         }
+
+        #endregion
     }
 }
